50c5bd749620348077d46e74617a831bc75ef641
sakibtamim <sakib.tamim@gmail.com>
2025-09-21 22:20:11 +0600

    adding winning lines and styles

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,54 @@ const App = () => {
     setWinner(null);
     setWinningLine([]);
   };
+
+  const getLineStyle = () => {
+    if (winningLine.length !== 3) return {};
+    const [a, b, c] = winningLine;
+
+    // Horizontal
+    if (a === 0 && b === 1 && c === 2)
+      return { top: "290px", left: "5px", width: "98%", height: "6px" };
+    if (a === 3 && b === 4 && c === 5)
+      return { top: "490px", left: "5px", width: "98%", height: "6px" };
+    if (a === 6 && b === 7 && c === 8)
+      return { top: "690px", left: "5px", width: "98%", height: "6px" };
+
+    // Vertical
+    if (a === 0 && b === 3 && c === 6)
+      return { top: "185px", left: "100px", width: "6px", height: "98%" };
+    if (a === 1 && b === 4 && c === 7)
+      return { top: "185px", left: "300px", width: "6px", height: "98%" };
+    if (a === 2 && b === 5 && c === 8)
+      return { top: "185px", left: "500px", width: "6px", height: "98%" };
+
+    // Diagonal
+    if (a === 0 && b === 4 && c === 8)
+      return {
+        top: "280px",
+        right: "500px",
+        width: "6px",
+        height: "100%",
+        transform: "rotate(-45deg)",
+        transformOrigin: "top right",
+      };
+    if (a === 2 && b === 4 && c === 6)
+      return {
+        top: "280px",
+        left: "500px",
+        width: "6px",
+        height: "100%",
+        transform: "rotate(45deg)",
+        transformOrigin: "top left",
+      };
+
+    return {};
+  };
+
   return (
-    <div className="w-[600px] h-[600px] mx-auto mt-[400px]">
-      <div className="pb-[60px]">
-        <p className="text-[80px]">
+    <div className="lg:w-[600px] w-[450px] lg:h-[600px] h-[450px] mx-auto lg:mt-[400px] mt-[50px] relative">
+      <div className="lg:pb-[60px] pb-[40px]">
+        <p className="lg:text-[80px] text-[50px]">
           {winner === "Draw"
             ? "It's a Draw!"
             : winner
@@ -62,10 +106,10 @@ const App = () => {
         <div className="flex">
           <div
             onClick={() => handleClick(0)}
-            className="w-[200px] h-[200px] bg-primary  border-l-5 border-t-5 border-white"
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-primary  lg:border-l-5 border-l-3 lg:border-t-5 border-t-3 border-white"
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[0] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -74,10 +118,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(1)}
-            className="w-[200px] h-[200px] bg-seconday border-l-5 border-t-5 border-r-5    border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-seconday lg:border-l-5 border-l-3 lg:border-t-5 border-t-3 lg:border-r-5 border-r-3    border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[1] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -86,10 +130,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(2)}
-            className="w-[200px] h-[200px] bg-primary  border-r-5 border-t-5 border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-primary  lg:border-r-5 border-r-3 lg:border-t-5 border-t-3 border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[2] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -100,10 +144,10 @@ const App = () => {
         <div className="flex">
           <div
             onClick={() => handleClick(3)}
-            className="w-[200px] h-[200px] bg-seconday  border-l-5 border-t-5 border-white"
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-seconday  lg:border-l-5 border-l-3 lg:border-t-5 border-t-3 border-white"
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[3] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -112,10 +156,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(4)}
-            className="w-[200px] h-[200px] bg-primary border-l-5 border-t-5 border-r-5 border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-primary lg:border-l-5 border-l-3 lg:border-t-5 border-t-3 lg:border-r-5 border-r-3 border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[4] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -124,10 +168,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(5)}
-            className="w-[200px] h-[200px] bg-seconday  border-r-5 border-t-5 border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-seconday  lg:border-r-5 border-r-3 lg:border-t-5 border-t-3 border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[5] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -138,10 +182,10 @@ const App = () => {
         <div className="flex">
           <div
             onClick={() => handleClick(6)}
-            className="w-[200px] h-[200px] bg-primary border-b-5 border-l-5 border-t-5 border-white"
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-primary lg:border-b-5 border-b-3 lg:border-l-5 border-l-3 lg:border-t-5 border-t-3 border-white"
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[6] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -150,10 +194,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(7)}
-            className="w-[200px] h-[200px] bg-seconday border-5 border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-seconday border-5 border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[7] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -162,10 +206,10 @@ const App = () => {
           </div>
           <div
             onClick={() => handleClick(8)}
-            className="w-[200px] h-[200px] bg-primary border-b-5 border-r-5 border-t-5 border-white "
+            className="lg:w-[200px] w-[150px] lg:h-[200px] h-[150px] bg-primary lg:border-b-5 border-b-3 lg:border-r-5 border-r-3 lg:border-t-5 border-t-3 border-white "
           >
             <p
-              className={`text-[150px] text-center leading-[200px] ${
+              className={`lg:text-[150px] text-[100px] text-center lg:leading-[200px] leading-[150px] ${
                 board[8] === "X" ? "text-x" : "text-o"
               }`}
             >
@@ -174,9 +218,15 @@ const App = () => {
           </div>
         </div>
       </div>
+      {winningLine.length === 3 && (
+        <div
+          className="absolute bg-winner z-10 lg:block hidden"
+          style={getLineStyle()}
+        ></div>
+      )}
       <div
         onClick={handleReset}
-        className="mt-[60px] text-[24px] py-[6px] px-[12px] bg-black text-white cursor-pointer inline-block"
+        className="mt-[60px] lg:text-[24px] text-[16px] py-[6px] px-[12px] bg-black text-white cursor-pointer inline-block"
       >
         Reset
       </div>
